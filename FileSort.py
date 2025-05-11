5786655e469ffa45295c10c3e55241838708a49a
ikarsbumblis <Ikars-Arkadijs.Bumblis@edu.rtu.lv>
2025-05-11 22:20:19 +0300

    Update FileSort.py
    Mapju izveides funkcija

--- FileSort.py
+++ FileSort.py
@@ -6,3 +6,4 @@ import shutil
 def load_config(file_name="config.json"):
     with open (file_name, "r", encoding="uts-8") as f:
         return json.load(f)
+def ensure_target_folders(base_path, extension_map):
