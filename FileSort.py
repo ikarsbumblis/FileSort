dafa03b4a1074c1027c1c31b0da783036d6b2858
ikarsbumblis <Ikars-Arkadijs.Bumblis@edu.rtu.lv>
2025-05-11 21:47:07 +0300

    Update FileSort.py
    Konfigurācijas faila nolasīšana

--- FileSort.py
+++ FileSort.py
@@ -1,3 +1,8 @@
 import os
 import json
 import shutil
+
+#config faila nolasīšana
+def load_config(file_name="config.json"):
+    with open (file_name, "r", encoding="uts-8") as f:
+        return json.load(f)
