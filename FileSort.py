57f615018fb7df7a1bfea35e480508cceac139f0
ikarsbumblis <Ikars-Arkadijs.Bumblis@edu.rtu.lv>
2025-05-11 22:53:07 +0300

    Update FileSort.py

--- FileSort.py
+++ FileSort.py
@@ -1,6 +1,7 @@
 import os
 import json
 import shutil
+from datetime import datetime
 
 #config faila nolasīšana
 def load_config(file_name="config.json"):
@@ -11,3 +12,10 @@ def ensure_target_folders(base_path, extension_map):
     for folder in target_folders:
         full_path = os.path.join(base_path, folder)
         os.makedirs(full_path, exist_ok=True)
+def move_files(config):
+    source = config["source_folder"]
+    ext_map = config["extension_map"]
+    
+    with open("log.txt", "a", encoding="utf-8") as log:
+        for file_name in os.listdir(source):
+            full_path = os.path.join(source, file_name)
